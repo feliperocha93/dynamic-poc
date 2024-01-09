85c08901d50cda2ddbed0bcaeb30d3179e72a80a
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2024-01-09 09:53:58 -0300

    feat: show chunks in console

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,8 @@ class AppHead extends Head {
   getDynamicChunks(files) {
     const dynamicChunks = super.getDynamicChunks(files);
 
+    console.log({ dynamicChunks });
+
     // Filtra os chunks que não têm o prefixo desejado
     return dynamicChunks.filter(({ key }) => !key.includes('lazy_'));
   }
